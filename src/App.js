f49c1e13d4050f62bfa894c924bc96bc315e7323
retnaphut <phutret@gmail.com>
2026-07-03 01:51:01 +0700

    Fix: downgrade React to 18.3.1 to ensure clean native compatibility and resolve Vercel build failures

--- src/App.js
+++ src/App.js
@@ -120,6 +120,7 @@ function App() {
         clearInterval(pollingInterval.current);
       }
     };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [apiUrl]);
 
   // Auto scroll active logs console to bottom
